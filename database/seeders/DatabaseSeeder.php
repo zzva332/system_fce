a0358c83099e850ce1c6a3a535b1d0defcfb56da
zzva332 <zzva332@gmail.com>
2025-02-09 14:56:24 -0500

    creacion de primer usuario admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,12 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        /*\App\Models\User::factory(10)->create();
+        User::factory(50)->create();
 
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => bcrypt('test123')
-        ]);*/
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin123')
+        ]);
     }
 }
